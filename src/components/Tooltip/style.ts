d773b0d3e74c312fcb09e0a1193fe1bbece315d6
Benji Visser <benji@093b.org>
2024-04-04 03:48:01 +0000

    Upgrade dependencies

--- src/components/Tooltip/style.ts
+++ src/components/Tooltip/style.ts
@@ -10,7 +10,7 @@ import { grayDarker, white } from '../style/colors'
 
 export const TooltipWrapper = styled.div``
 
-export const TooltipStyled: any = styled(Tooltip)`
+export const TooltipStyled: any = styled(Tooltip)<{ opacity: number | string }>`
   opacity: ${(props) => props.opacity.toString()};
 `
 
